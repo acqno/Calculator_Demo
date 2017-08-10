096a39442b85f3a9d3702bd8ad583a0d1eab4c1b
alvin <alvin@DESKTOP-391GE8T>
2017-08-09 23:38:59 -0400

    Created SplashForm

---
+++ CalculatorDemo/SplashForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Name: Alvun Quijano
+ * Date: Aug 09, 2017
+ * Desc: This is the SplashForm class
+ * Ver: 0.1 - Created the SplashForm
+ */
+
+namespace CalculatorDemo
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
